f7382f1b8e205f83961e14f7c16234a91253b580
PrasannaGurram2505 <prasannagurram2505@gmail.com>
2025-12-02 18:19:52 +0530

    Update App.java

--- 3-java-jenkins-docker-app/src/main/java/com/example/App.java
+++ 3-java-jenkins-docker-app/src/main/java/com/example/App.java
@@ -1,7 +1,24 @@
-package com.example;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+package com.example
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting Simple HTTP Server...");
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java Application for Jenkins CI/CD!");
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello from Java App Running in Docker!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server running on http://localhost:8080/");
     }
 }
